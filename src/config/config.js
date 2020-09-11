e36f6aad760991628b3803e2b2d1c7118a78e116
oskarincon <oskarincon22@gmail.com>
2020-09-11 00:18:09 -0500

    changed config

--- src/config/config.js
+++ src/config/config.js
@@ -4,10 +4,10 @@ process.env.PORT = process.env.PORT || 3000
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
-let urlDb;
+let urldb;
 if (node_env = 'dev')
-    urlDb = 'mongodb://localhost:27017/cafe'
+    urldb = 'mongodb://localhost:27017/cafe'
 else
-    urlDb = 'mongodb+srv://oskarincon:kYd0yUkdfE4nIyZi@cluster0.h6zvg.mongodb.net/test'
+    urldb = 'mongodb+srv://oskarincon:kYd0yUkdfE4nIyZi@cluster0.h6zvg.mongodb.net/test'
 
-process.env.NODE_ENV = urlDb;
+process.env.NODE_ENV = urldb;
